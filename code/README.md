587b779553168d538e1051da6ff20afdd9b3862d
jacobhsu <jacob.hsu.tw@gmail.com>
2019-04-30 14:07:19 +0800

    init tail

--- code/README.md
+++ code/README.md
@@ -10,6 +10,8 @@ ramda [append](http://ramda.cn/docs/#append)
 `$ code> node list`  
 [head](http://ramda.cn/docs/#head) 求列表或字符串的首個元素
 [last](http://ramda.cn/docs/#last) 返回列表或字符串的最後一個元素  
+[init](http://ramda.cn/docs/#init) 返回 list 或 string 刪除最後一個元素後的部分  
+[tail](http://ramda.cn/docs/#tail) 刪除列表中的首個元素  
 
 ## Function  
 [addIndex](http://ramda.cn/docs/#addIndex)  創建新的列表迭代函數  
--- code/list.js
+++ code/list.js
@@ -13,3 +13,17 @@ let lastEmptyArr = R.last([]); //=> undefined
 let lastStr = R.last('abc'); //=> 'c'
 let lastStrNull = R.last(''); //=> ''
 console.log(lastArr, lastEmptyArr, lastStr, lastStrNull)
+
+let initArr = R.init([1, 2, 3]);  //=> [1, 2]
+let initArr1 = R.init([1]);        //=> []
+
+let initStr = R.init('abc');  //=> 'ab'
+let initStr1 = R.init('a');    //=> ''
+console.log(initArr, initArr1, initStr, initStr1)
+
+let tailArr = R.tail([1, 2, 3]);  //=> [2, 3]
+let tailArr1 = R.tail([1]);        //=> []
+
+let tailStr = R.tail('abc');  //=> 'bc'
+let tailStr1 = R.tail('a');    //=> ''
+console.log(tailArr, tailArr1, tailStr, tailStr1)
