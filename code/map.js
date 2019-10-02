cf09c45e82191310dfeae6044dcfe705c6359e7f
jacobhsu <jacob.hsu.tw@gmail.com>
2019-10-02 18:13:32 +0800

    R.map and  el-select

--- code/map.js
+++ code/map.js
@@ -1,6 +1,6 @@
 const R = require('ramda');
 
-const double = x => x * 2;
+const double = x => {x * 2; console.log(x);};
 let map1 = R.map(double, [1, 2, 3]); //=> [2, 4, 6]
 let map2 = R.map(double, {x: 1, y: 2, z: 3}); //=> {x: 2, y: 4, z: 6}
 console.log(map1 , map2);
